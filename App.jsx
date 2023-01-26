b09311263f978254f5c2edc43f74d0fbb2630901
Ilyes98 <belfekihilyess@gmail.com>
2023-01-26 14:11:31 +0100

    merge

--- App.jsx
+++ App.jsx
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, LogBox, Text, View } from 'react-native';
+import { COLORS } from './constants/theme.js';
+import { user } from './data/user.js';
+export { user as profile } from './data/user.js';
 
 // Hide Error FlatList using inside ScrollView
 LogBox.ignoreLogs(['VirtualizedLists']);
 
-
+//test
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> {user.name}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,7 +20,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: COLORS.black,
     alignItems: 'center',
     justifyContent: 'center',
   },
