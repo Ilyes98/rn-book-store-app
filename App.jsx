c2ba68e9ff7ee79877e84ecb1661b4afda4b6644
Ilyes98 <belfekihilyess@gmail.com>
2023-01-26 16:01:59 +0100

    add book section

--- App.jsx
+++ App.jsx
@@ -1,18 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, LogBox, Text, View } from 'react-native';
-
-import { Body } from './components/Body';
+import { View } from 'react-native';
 import { Header } from './components/Header';
-import { COLORS } from './constants/theme';
+import { Body } from './components/Body';
 import { styles } from './css/styles';
-// Hide Error FlatList using inside ScrollView
-LogBox.ignoreLogs(['VirtualizedLists']);
+import BookSection from './components/BookSection';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
-      <Body />
-    </View>
+  <BookSection />
+  <Body />
+  </View>
   );
 }
