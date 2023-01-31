9aa14570441e080635fee03bf2c6729aba770506
YL <yass_official@outlook.fr>
2023-01-31 13:40:46 +0100

    Add BookSection in body and add scroll

--- components/Body.jsx
+++ components/Body.jsx
@@ -1,12 +1,17 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { styles } from "../css/styles";
+import BookSection from "./BookSection";
 
 export function Body() {
     return (
     <View style={styles.body}>
+       <ScrollView>
+       <BookSection />
+       
       <Text style={{
         color: 'white'
       }}>This is a body</Text>
+      </ScrollView>
     </View>
     );
   }
